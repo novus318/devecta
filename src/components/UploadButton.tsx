fb2176c23a83d4c2102fd34b529b31e55fa2a12d
novus318 <nizamudheen318@gmail.com>
2024-06-30 12:06:28 +0530

    error clear

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -84,7 +84,8 @@ if(!key){
     }}>
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <section>
-          <div {...getRootProps()} className='border h-64 m-3 border-dashed border-gray-300 rounded-lg'>
+          <div {...getRootProps()} className='border h-64 m-3 border-dashed border-gray-300 rounded-lg'
+          onClick={(e)=> e.stopPropagation()}>
             <div className='flex items-center justify-center h-full w-full'>
               <label htmlFor="dropzone-file" className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
                 <div className='flex flex-col items-center pt-5 pb-6 justify-center'>
